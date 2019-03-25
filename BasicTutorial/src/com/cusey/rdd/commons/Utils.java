efec83b05ceea48227c96471cb3ba16d8507b684
john cusey <john_cusey@hotmail.com>
2019-03-25 18:39:23 -0400

    update

--- BasicTutorial/src/com/cusey/rdd/commons/Utils.java
+++ BasicTutorial/src/com/cusey/rdd/commons/Utils.java
@@ -8,4 +8,3 @@ public class Utils {
     // a regular expression which matches commas but not commas within double quotations
     public static final String COMMA_DELIMITER = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
 }
-
